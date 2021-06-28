d73b7678c410e451ca3a39e71ed72ae11f631de5
DESKTOP-82QBQO9\iForensics <swing0313@yahoo.com.tw>
2021-06-28 15:17:58 +0800

    update port

--- nodelogin/login.js
+++ nodelogin/login.js
@@ -283,7 +283,7 @@ function sendNotifyMessage (msg, token) {
     return request;
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
